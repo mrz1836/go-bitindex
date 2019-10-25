5f6f31f5722ab338a884446c782c634fc54bd801
mrz1836 <mrz1818@gmail.com>
2019-10-25 19:48:34 -0400

    First base of the project, config, definitions, address requests

---
+++ bitindex.go
@@ -0,0 +1,240 @@
+/*
+Package bitindex is the unofficial golang implementation for the bitindex API
+
+Example:
+
+// Create a client
+client, _ := bitindex.NewClient()
+*/
+package bitindex
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gojek/heimdall"
+	"github.com/gojek/heimdall/httpclient"
+)
+
+// Client holds client configuration settings
+type Client struct {
+
+	// HTTPClient carries out the POST operations
+	HTTPClient heimdall.Client
+
+	// Parameters contains the search parameters that are submitted with your query,
+	// which may affect the data returned
+	Parameters *RequestParameters
+
+	// LastRequest is the raw information from the last request
+	LastRequest *LastRequest
+}
+
+// RequestParameters holds options that can affect data returned by a request.
+type RequestParameters struct {
+
+	// UserAgent (optional for changing user agents)
+	UserAgent string
+
+	// Network is what this search should use IE: main
+	Network NetworkType
+
+	// APIKey is the api key to use
+	APIKey string
+}
+
+// LastRequest is used to track what was submitted to the Request()
+type LastRequest struct {
+
+	// Method is either POST or GET
+	Method string
+
+	// PostData is the post data submitted if POST request
+	PostData string
+
+	// StatusCode is the last code from the request
+	StatusCode int
+
+	// URL is the url used for the request
+	URL string
+}
+
+// NewClient creates a new client to submit queries with.
+// Parameters values are set to the defaults defined by WhatsOnChain.
+//
+// For more information: https://www.bitindex.network/developers/api-documentation-v3.html#Authentication
+func NewClient(apiKey string) (c *Client, err error) {
+
+	// Make sure we have an API key
+	if len(apiKey) == 0 {
+		err = fmt.Errorf("missing required api key")
+		return
+	}
+
+	// Create a client
+	c = new(Client)
+
+	// Create exponential backoff
+	backOff := heimdall.NewExponentialBackoff(
+		ConnectionInitialTimeout,
+		ConnectionMaxTimeout,
+		ConnectionExponentFactor,
+		ConnectionMaximumJitterInterval,
+	)
+
+	// Create the http client
+	//c.HTTPClient = new(http.Client) (@mrz this was the original HTTP client)
+	c.HTTPClient = httpclient.NewClient(
+		httpclient.WithHTTPTimeout(ConnectionWithHTTPTimeout),
+		httpclient.WithRetrier(heimdall.NewRetrier(backOff)),
+		httpclient.WithRetryCount(ConnectionRetryCount),
+		httpclient.WithHTTPClient(&http.Client{
+			Transport: ClientDefaultTransport,
+		}),
+	)
+
+	// Create default parameters
+	c.Parameters = new(RequestParameters)
+	c.Parameters.APIKey = apiKey
+	c.Parameters.Network = NetworkMain
+	c.Parameters.UserAgent = DefaultUserAgent
+
+	// Create a last request struct
+	c.LastRequest = new(LastRequest)
+
+	// Return the client
+	return
+}
+
+// Request is a generic bitindex request wrapper that can be used without constraints
+func (c *Client) Request(endpoint string, method string, payload []byte) (response string, err error) {
+
+	// Set reader
+	var bodyReader io.Reader
+
+	// Add the network value
+	endpoint = fmt.Sprintf("%s%s/%s", APIEndpoint, c.Parameters.Network, endpoint)
+
+	// Switch on POST vs GET
+	switch method {
+	case "POST":
+		{
+			bodyReader = bytes.NewBuffer(payload)
+		}
+	}
+
+	// Store for debugging purposes
+	c.LastRequest.Method = method
+	c.LastRequest.URL = endpoint
+
+	// Start the request
+	var request *http.Request
+	if request, err = http.NewRequest(method, endpoint, bodyReader); err != nil {
+		return
+	}
+
+	// Change the header (user agent is in case they block default Go user agents)
+	request.Header.Set("User-Agent", c.Parameters.UserAgent)
+	request.Header.Set("api_key", c.Parameters.APIKey)
+
+	// Set the content type on POST
+	if method == "POST" {
+		request.Header.Set("Content-Type", "application/json")
+	}
+
+	// Fire the http request
+	var resp *http.Response
+	if resp, err = c.HTTPClient.Do(request); err != nil {
+		return
+	}
+
+	// Close the response body
+	defer func() {
+		if bodyErr := resp.Body.Close(); bodyErr != nil {
+			log.Printf("error closing response body: %s", bodyErr.Error())
+		}
+	}()
+
+	// Save the status
+	c.LastRequest.StatusCode = resp.StatusCode
+
+	// Read the body
+	var body []byte
+	if body, err = ioutil.ReadAll(resp.Body); err != nil {
+		return
+	}
+
+	// Parse the response
+	response = string(body)
+
+	// Done
+	return
+}
+
+// AddressInfo this endpoint retrieves various address info.
+//
+// Form more information: https://www.bitindex.network/developers/api-documentation-v3.html#Address
+func (c *Client) AddressInfo(address string) (addressInfo *AddressInfo, err error) {
+
+	var resp string
+	// /api/v3/network/addr/address
+	resp, err = c.Request("addr/"+address, "GET", nil)
+	if err != nil {
+		return
+	}
+
+	addressInfo = new(AddressInfo)
+	if err = json.Unmarshal([]byte(resp), addressInfo); err != nil {
+		return
+	}
+	return
+}
+
+// AddressUnspentTransactions this endpoint retrieves ordered list of UTXOs.
+//
+// Form more information: https://www.bitindex.network/developers/api-documentation-v3.html#Address
+func (c *Client) AddressUnspentTransactions(address string) (transactions UnspentTransactions, err error) {
+
+	var resp string
+	// /api/v3/network/addr/address/utxo
+	resp, err = c.Request("addr/"+address+"/utxo", "GET", nil)
+	if err != nil {
+		return
+	}
+
+	transactions = *new(UnspentTransactions)
+	if err = json.Unmarshal([]byte(resp), &transactions); err != nil {
+		return
+	}
+	return
+}
+
+// GetTransactions this endpoint retrieves ordered list of transactions.
+//
+// Form more information: https://www.bitindex.network/developers/api-documentation-v3.html#Address
+func (c *Client) GetTransactions(transactionRequest *GetTransactionsRequest) (response GetTransactionsResponse, err error) {
+
+	var data []byte
+	data, err = json.Marshal(transactionRequest)
+	if err != nil {
+		return
+	}
+
+	var resp string
+	// /api/v3/network/addrs/txs
+	resp, err = c.Request("addrs/txs", "POST", data)
+	if err != nil {
+		return
+	}
+
+	response = *new(GetTransactionsResponse)
+	if err = json.Unmarshal([]byte(resp), &response); err != nil {
+		return
+	}
+	return
+}
---
+++ definitions.go
@@ -0,0 +1,117 @@
+package bitindex
+
+// NetworkType is used internally to represent the possible values
+// for network in queries to be submitted: {"main", "test", "stn"}
+type NetworkType string
+
+// AddressInfo is the address info for a returned address request
+type AddressInfo struct {
+	Address                    string   `json:"addrStr"`
+	Balance                    float64  `json:"balance"`
+	BalanceSatoshis            int64    `json:"balanceSat"`
+	TotalReceived              float64  `json:"totalReceived"`
+	TotalReceivedSatoshis      int64    `json:"totalReceivedSat"`
+	TotalSent                  float64  `json:"totalSent"`
+	TotalSentSatoshis          int64    `json:"totalSentSat"`
+	Transactions               []string `json:"transactions"`
+	TxAppearances              int64    `json:"txApperances"`
+	UnconfirmedBalance         float64  `json:"unconfirmedBalance"`
+	UnconfirmedBalanceSatoshis int64    `json:"unconfirmedBalanceSat"`
+	UnconfirmedTxAppearances   int64    `json:"unconfirmedTxApperances"`
+}
+
+// UnspentTransactions is a list of unspent transactions
+type UnspentTransactions []UnspentTransaction
+
+// UnspentTransaction is a standard UTXO response
+type UnspentTransaction struct {
+	Address       string  `json:"address"`
+	Amount        float64 `json:"amount"`
+	Confirmations int64   `json:"confirmations"`
+	Height        int64   `json:"height"`
+	OutputIndex   int64   `json:"outputIndex"`
+	Satoshis      int64   `json:"satoshis"`
+	Script        string  `json:"script"`
+	ScriptPubKey  string  `json:"scriptPubKey"`
+	TxID          string  `json:"txid"`
+	Value         int64   `json:"value"`
+	Vout          int     `json:"vout"`
+}
+
+// GetTransactionsRequest is for making a POST to get transactions
+type GetTransactionsRequest struct {
+	Address        string `json:"addrs"` // addr1,addr2,addr3
+	AfterBlockHash string `json:"afterBlockHash,omitempty"`
+	AfterHeight    string `json:"afterHeight,omitempty"`
+	FromIndex      int64  `json:"fromIndex,omitempty"`
+	IncludeAsm     bool   `json:"includeAsm"`
+	IncludeHex     bool   `json:"includeHex"`
+	ToIndex        int64  `json:"toIndex,omitempty"`
+}
+
+// GetTransactionsResponse is the response from the POST request
+type GetTransactionsResponse struct {
+	TotalItems int64         `json:"totalItems"`
+	From       int64         `json:"from"`
+	To         int64         `json:"to"`
+	Items      []Transaction `json:"items"`
+}
+
+// Transaction is returned in the GetTransactionsResponse
+type Transaction struct {
+	BlockHash     string       `json:"blockhash"`
+	BlockHeight   int64        `json:"blockheight"`
+	BlockTime     int64        `json:"blocktime"`
+	Confirmations int64        `json:"confirmations"`
+	Fees          float64      `json:"fees"`
+	Hash          string       `json:"hash"`
+	LockTime      int64        `json:"locktime"`
+	RawTx         string       `json:"rawtx"`
+	Size          int64        `json:"size"`
+	Time          int64        `json:"time"`
+	TxID          string       `json:"txid"`
+	ValueIn       float64      `json:"valueIn"`
+	ValueOut      float64      `json:"valueOut"`
+	Version       int          `json:"version"`
+	Vin           []VinObject  `json:"vin"`
+	Vout          []VoutObject `json:"vout"`
+}
+
+// VinObject is the vin data
+type VinObject struct {
+	Address       string          `json:"address"`
+	AddressAddr   string          `json:"addr"`
+	N             int             `json:"n"`
+	ScriptSig     ScriptSigObject `json:"scriptSig"`
+	Sequence      int64           `json:"sequence"`
+	TxID          string          `json:"txid"`
+	Value         float64         `json:"value"`
+	ValueSatoshis int64           `json:"valueSat"`
+	Vout          int             `json:"vout"`
+}
+
+// ScriptSigObject is the script signature data
+type ScriptSigObject struct {
+	Asm string `json:"asm"`
+	Hex string `json:"hex"`
+}
+
+// VoutObject is the vout data
+type VoutObject struct {
+	N             int                `json:"n"`
+	ScriptPubKey  ScriptPubKeyObject `json:"scriptPubKey"`
+	SpentHeight   int64              `json:"spentHeight"`
+	SpentIndex    int64              `json:"spentIndex"`
+	SpentTxID     string             `json:"spentTxId"`
+	Value         float64            `json:"value"`
+	ValueSatoshis int64              `json:"valueSat"`
+}
+
+// ScriptPubKeyObject is the script pubkey data
+type ScriptPubKeyObject struct {
+	Addresses          []string `json:"addresses"`
+	Asm                string   `json:"asm"`
+	Hex                string   `json:"hex"`
+	RequiredSignatures int      `json:"reqSigs"`
+	Type               string   `json:"type"`
+}
