fe372b9dc8728ea19ab1da0c8e066b78614ca285
mrz1836 <mrz1818@gmail.com>
2019-10-26 15:36:30 -0400

    Base test methods

---
+++ bitindex_test.go
@@ -0,0 +1,37 @@
+package bitindex
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+var (
+	testAPIKey = os.Getenv("BITINDEX_API_KEY")
+)
+
+// TestNewClient test new client
+func TestNewClient(t *testing.T) {
+	client, err := NewClient(testAPIKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if client.Parameters.Network != NetworkMain {
+		t.Fatal("expected value to be main")
+	}
+}
+
+// ExampleNewClient example using NewClient()
+func ExampleNewClient() {
+	client, _ := NewClient(testAPIKey)
+	fmt.Println(client.Parameters.Network)
+	// Output:main
+}
+
+// BenchmarkNewClient benchmarks the NewClient method
+func BenchmarkNewClient(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _ = NewClient(testAPIKey)
+	}
+}
