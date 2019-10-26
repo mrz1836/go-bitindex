138e7655c64fd506ebe379e17bcfb4d31aa787be
mrz1836 <mrz1818@gmail.com>
2019-10-26 15:34:22 -0400

    Ported the MVP for chain functionality

---
+++ chain.go
@@ -0,0 +1,82 @@
+package bitindex
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// ChainInfo this endpoint retrieves the current chain info.
+//
+// For more information: https://www.bitindex.network/developers/api-documentation-v3.html#ChainInfo
+func (c *Client) ChainInfo() (chainInfo *ChainInfoResponse, err error) {
+
+	var resp string
+	// /api/v3/network/status
+	resp, err = c.Request("status?q=chainInfo", http.MethodGet, nil)
+	if err != nil {
+		return
+	}
+
+	chainInfo = new(ChainInfoResponse)
+	if err = json.Unmarshal([]byte(resp), chainInfo); err != nil {
+		return
+	}
+	return
+}
+
+// ChainDifficulty this endpoint retrieves the current chain difficulty.
+//
+// For more information: https://www.bitindex.network/developers/api-documentation-v3.html#ChainInfo
+func (c *Client) ChainDifficulty() (difficulty *ChainDifficultyResponse, err error) {
+
+	var resp string
+	// /api/v3/network/status
+	resp, err = c.Request("status?q=getDifficulty", http.MethodGet, nil)
+	if err != nil {
+		return
+	}
+
+	difficulty = new(ChainDifficultyResponse)
+	if err = json.Unmarshal([]byte(resp), difficulty); err != nil {
+		return
+	}
+	return
+}
+
+// ChainBestBlockHash this endpoint retrieves the current best block hash
+//
+// For more information: https://www.bitindex.network/developers/api-documentation-v3.html#ChainInfo
+func (c *Client) ChainBestBlockHash() (bestBlockHash *ChainBestBlockHashResponse, err error) {
+
+	var resp string
+	// /api/v3/network/status
+	resp, err = c.Request("status?q=getBestBlockHash", http.MethodGet, nil)
+	if err != nil {
+		return
+	}
+
+	bestBlockHash = new(ChainBestBlockHashResponse)
+	if err = json.Unmarshal([]byte(resp), bestBlockHash); err != nil {
+		return
+	}
+	return
+}
+
+// ChainLastBlockHash this endpoint retrieves the last block hash
+//
+// For more information: https://www.bitindex.network/developers/api-documentation-v3.html#ChainInfo
+func (c *Client) ChainLastBlockHash() (lastBlockHash *ChainLastBlockHashResponse, err error) {
+
+	var resp string
+	// /api/v3/network/status
+	resp, err = c.Request("status?q=getLastBlockHash", http.MethodGet, nil)
+	if err != nil {
+		return
+	}
+
+	lastBlockHash = new(ChainLastBlockHashResponse)
+	if err = json.Unmarshal([]byte(resp), lastBlockHash); err != nil {
+		return
+	}
+	return
+}
---
+++ chain_test.go
@@ -0,0 +1,103 @@
+package bitindex
+
+import "testing"
+
+// TestClient_ChainInfo tests the ChainInfo()
+func TestClient_ChainInfo(t *testing.T) {
+	// Skip this test in short mode (not needed)
+	if testing.Short() {
+		t.Skip("skipping testing in short mode")
+	}
+
+	// Create a new client object to handle your queries (supply an API Key)
+	client, err := NewClient(testAPIKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var resp *ChainInfoResponse
+	resp, err = client.ChainInfo()
+	if err != nil {
+		t.Fatal("error occurred: " + err.Error())
+	}
+
+	if resp.Info.Difficulty == 0 {
+		t.Fatal("we should have the Difficulty ", resp.Info.Difficulty)
+	}
+}
+
+// TestClient_ChainDifficulty tests the ChainDifficulty()
+func TestClient_ChainDifficulty(t *testing.T) {
+	// Skip this test in short mode (not needed)
+	if testing.Short() {
+		t.Skip("skipping testing in short mode")
+	}
+
+	// Create a new client object to handle your queries (supply an API Key)
+	client, err := NewClient(testAPIKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var resp *ChainDifficultyResponse
+	resp, err = client.ChainDifficulty()
+	if err != nil {
+		t.Fatal("error occurred: " + err.Error())
+	}
+
+	if resp.Difficulty == 0 {
+		t.Fatal("we should have the Difficulty ", resp.Difficulty)
+	}
+}
+
+// TestClient_ChainBestBlockHash tests the ChainBestBlockHash()
+func TestClient_ChainBestBlockHash(t *testing.T) {
+	// Skip this test in short mode (not needed)
+	if testing.Short() {
+		t.Skip("skipping testing in short mode")
+	}
+
+	// Create a new client object to handle your queries (supply an API Key)
+	client, err := NewClient(testAPIKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var resp *ChainBestBlockHashResponse
+	resp, err = client.ChainBestBlockHash()
+	if err != nil {
+		t.Fatal("error occurred: " + err.Error())
+	}
+
+	if len(resp.BestBlockHash) == 0 {
+		t.Fatal("we should have a hash ", resp.BestBlockHash)
+	}
+}
+
+// TestClient_ChainLastBlockHash tests the ChainLastBlockHash()
+func TestClient_ChainLastBlockHash(t *testing.T) {
+	// Skip this test in short mode (not needed)
+	if testing.Short() {
+		t.Skip("skipping testing in short mode")
+	}
+
+	// Create a new client object to handle your queries (supply an API Key)
+	client, err := NewClient(testAPIKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var resp *ChainLastBlockHashResponse
+	resp, err = client.ChainLastBlockHash()
+	if err != nil {
+		t.Fatal("error occurred: " + err.Error())
+	}
+
+	if len(resp.LastBlockHash) == 0 {
+		t.Fatal("we should have a hash ", resp.LastBlockHash)
+	}
+
+	if len(resp.SyncTipHash) == 0 {
+		t.Fatal("we should have a tip ", resp.SyncTipHash)
+	}
+}
