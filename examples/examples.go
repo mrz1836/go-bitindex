da5b78eca6bbb98c5ec1b6890bf2cd68fb364b2e
mrz1836 <mrz1818@gmail.com>
2019-10-26 17:26:53 -0400

    Working example for this package

---
+++ examples/examples.go
@@ -0,0 +1,34 @@
+/*
+Package main is an example package that uses go-bitindex
+*/
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/mrz1836/go-bitindex"
+)
+
+// main is an example of using this go-bitindex package
+func main() {
+
+	// Set your api key
+	yourAPIKey := os.Getenv("BITINDEX_API_KEY")
+
+	// Create a new client
+	client, err := bitindex.NewClient(yourAPIKey)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// Get balance for an address
+	var info *bitindex.AddressInfo
+	info, err = client.AddressInfo("16ZqP5Tb22KJuvSAbjNkoiZs13mmRmexZA")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// What's the balance?
+	log.Println("address balance:", info.Balance)
+}
